12d3991ce5dc290220c87e7df4b6b79498571ca1
nurhafiza210 <nurhafiza364@gmail.com>
2024-05-30 09:30:12 +0700

    commit pertama

---
+++ src/hello.php
@@ -0,0 +1,11 @@
+<?php
+
+    namespace nurhafiza\labhello;
+
+    class hello{
+        public function tampilHello($nama){
+            return "Hello $nama selamat datang di library sederhana";
+        }
+    }
+
+?>
